4179071644cefac15acc7d3d3232c742cae983f1
like-cent <pasindulakshan3599@gmail.com>
2025-02-21 22:02:58 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
 SESSION_ID: process.env.SESSION_ID === undefined ? 'HANSAMAL-MD=R5w1xQaQ#uuISg6_-DAg_7Cm5rDcpcSsk0gyGwO4eTAbTfBtpHHI' : process.env.SESSION_ID,
-GITHUB_USER_NAME: process.env.GITHUB_USER_NAME === undefined ? '' : process.env.GITHUB_USER_NAME,
+GITHUB_USER_NAME: process.env.GITHUB_USER_NAME === undefined ? 'like-cent' : process.env.GITHUB_USER_NAME,
 GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN === undefined ? '' : process.env.GITHUB_AUTH_TOKEN,        
 OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '94711262551' : process.env.OWNER_NUMBER
 };
